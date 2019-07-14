e51bdcb09d167ff2139baa20572142f29f55f57f
Matt Weiss <weiss.matt@outlook.com>
2019-07-14 14:50:59 -0600

    Sets up create user mutation

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,30 @@
+module Mutations
+  class CreateUser < BaseMutation
+    argument :email, String, required: false
+    argument :nickname, String, required: true
+    argument :password, String, required: true
+    argument :role, Integer, required: true
+    argument :name, String, required: false
+    argument :phone_number, String, required: false
+
+    # return type from the mutation
+   type Types::UserType
+
+   def resolve(email: nil,
+            nickname: nil,
+            password: nil,
+                role: nil,
+                name: nil,
+        phone_number: nil)
+
+     User.create!(
+       email: email,
+       nickname: nickname,
+       password: password,
+           role: role,
+           name: name,
+   phone_number: phone_number
+     )
+   end
+  end
+end
