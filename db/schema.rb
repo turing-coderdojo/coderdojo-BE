d3b649226127cf2926aa18b1a1504f780a59c156
Matt Weiss <weiss.matt@outlook.com>
2019-07-15 16:22:28 -0600

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_203445) do
+ActiveRecord::Schema.define(version: 2019_07_15_220612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_07_13_203445) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "guardian_id"
+    t.index ["guardian_id"], name: "index_users_on_guardian_id"
   end
 
 end
