001cf5feb13b9f2ea30bbf1eb202bbd1c301c106
Matt Weiss <weiss.matt@outlook.com>
2019-07-15 23:02:42 -0600

    Adds fields for querying guardians and students

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -8,7 +8,8 @@ module Types
     field :name, String, null: true
     field :notes, String, null: true
     field :phone_number, String, null: true
-    field :guardian_id, Integer, null: true
+    field :guardian_id, UserType, null: true, method: :guardian
+    field :students, [UserType], null: true, method: :students
     field :created_at, String, null: false
     field :updated_at, String, null: false
   end
