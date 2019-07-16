0d64b173428e1e0e121e216e6180764f6bca9e04
Matt Weiss <weiss.matt@outlook.com>
2019-07-15 23:00:55 -0600

    Adds query field for "me" (current_user)

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,7 +3,11 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     field :all_users, [UserType], null: false
+    field :me, Types::UserType, null: true
 
+    def me
+      context[:current_user]
+    end
 
     def all_users
       User.all
