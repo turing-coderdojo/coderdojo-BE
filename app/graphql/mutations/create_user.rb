d454a01a99548a28ddc8a75a7c6bc37a0cb3b6dd
Matt Weiss <weiss.matt@outlook.com>
2019-07-16 11:58:52 -0600

    Adds address creation to user creation

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,18 +1,38 @@
 module Mutations
   class CreateUser < Mutations::BaseMutation
+    #User args
     argument :email, String, required: true
     argument :username, String, required: true
     argument :password, String, required: true
     argument :name, String, required: true
     argument :phone_number, String, required: true
+    #Address args
+    argument :street_1, String, required: true
+    argument :street_2, String, required: false
+    argument :city, String, required: true
+    argument :state, String, required: true
+    argument :zip, String, required: true
+
     # return type from the mutation
     type Types::UserType
 
     def resolve(email: nil,
-            username: nil,
-            password: nil,
-                name: nil,
-        phone_number: nil)
+             username: nil,
+             password: nil,
+                 name: nil,
+         phone_number: nil,
+             street_1: nil,
+             street_2: nil,
+                 city: nil,
+                state: nil,
+                  zip: nil)
+
+      address = Address.create!(
+        street_1: street_1,
+        street_2: street_2,
+            city: city,
+           state: state,
+             zip: zip)
 
       User.create!(
            email: email,
@@ -20,7 +40,8 @@ module Mutations
         password: password,
             role: 1,
             name: name,
-    phone_number: phone_number)
+    phone_number: phone_number,
+      address_id: address.id)
     end
   end
 end
