427eee846e26a3a27fc4f8784689c599e993ad1b
Matt Weiss <weiss.matt@outlook.com>
2019-07-16 11:32:40 -0600

    Adds addresses table

---
+++ db/migrate/20190716172813_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string :street_1
+      t.string :street_2
+      t.string :city
+      t.string :state
+      t.string :zip
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_171629) do
+ActiveRecord::Schema.define(version: 2019_07_16_172813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street_1"
+    t.string "street_2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
