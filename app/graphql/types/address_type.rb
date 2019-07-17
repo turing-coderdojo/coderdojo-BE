a75039576fbe0c8190839050bd276efc09b0285c
Matt Weiss <weiss.matt@outlook.com>
2019-07-17 14:23:54 -0600

    Adds address type

---
+++ app/graphql/types/address_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class AddressType < BaseObject
+    field :id, Integer, null: true
+    field :street_1, String, null: true
+    field :street_2, String, null: false
+    field :city, String, null: false
+    field :state, String, null: true
+    field :zip, String, null: true
+    field :addressable_type, String, null: true
+    field :addressable_id, String, null: true
+  end
+end
