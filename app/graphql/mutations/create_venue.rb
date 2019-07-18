e6bb7384ab6b1e7f49e9d7f4069ba6f03176f52a
Matt Weiss <weiss.matt@outlook.com>
2019-07-18 12:21:28 -0600

    Adds superuser conditional to create_venue

--- app/graphql/mutations/create_venue.rb
+++ app/graphql/mutations/create_venue.rb
@@ -1,10 +1,18 @@
 module Mutations
   class CreateVenue < Mutations::BaseMutation
+    #Venue args
     argument :name, String, required: true
     argument :email, String, required: false
     argument :notes, String, required: false
     argument :web_url, String, required: false
     argument :photo_url, String, required: false
+
+    #Address args
+    argument :street_1, String, required: true
+    argument :street_2, String, required: false
+    argument :city, String, required: true
+    argument :state, String, required: true
+    argument :zip, String, required: true
 
     type Types::VenueType
 
@@ -19,20 +27,23 @@ module Mutations
                state: nil,
                  zip: nil)
 
-      venue = Venue.create(name: name,
-                  email: email,
-                  notes: notes,
-                web_url: web_url,
-              photo_url: photo_url)
+      if context[:current_user] && (context[:current_user][:role] == 3)
+        venue = Venue.create(name: name,
+                    email: email,
+                    notes: notes,
+                  web_url: web_url,
+                photo_url: photo_url)
 
-      venue.addresses.create(
-        street_1: street1,
-        street_2: street2,
-            city: city,
-           state: state,
-             zip: zip)
-
-      return venue
+        venue.addresses.create(
+          street_1: street1,
+          street_2: street2,
+              city: city,
+             state: state,
+               zip: zip)
+        return venue
+      else
+        {error: "Not Authorized to create venue!"}
+      end
     end
   end
 end
