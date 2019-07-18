5f9defd957dbde4be52d9bf7eb4f4b1d40e91c5f
Matt Weiss <weiss.matt@outlook.com>
2019-07-17 23:00:36 -0600

    Adds mutation for venue creation

---
+++ app/graphql/mutations/create_venue.rb
@@ -0,0 +1,24 @@
+module Mutations
+  class CreateVenue < Mutations::BaseMutation
+    argument :name, String, required: true
+    argument :email, String, required: false
+    argument :notes, String, required: false
+    argument :web_url, String, required: false
+    argument :photo_url, String, required: false
+
+    type Types::VenueType
+
+    def resolve(name: nil,
+               email: nil,
+               notes: nil,
+             web_url: nil,
+           photo_url: nil)
+
+      Venue.create(name: name,
+                  email: email,
+                  notes: notes,
+                web_url: web_url,
+              photo_url: photo_url)
+    end
+  end
+end
