8252e9e2a55037c59ff464e339a963038e2a0a41
Matt Weiss <weiss.matt@outlook.com>
2019-07-18 12:22:15 -0600

    Adds sad path test

--- spec/mutations/create_venue_spec.rb
+++ spec/mutations/create_venue_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 class Mutations::CreateUserTest < ActiveSupport::TestCase
   describe 'GraphQL User mutations' do
-    def perform(venue: nil, **args)
-      Mutations::CreateVenue.new(object: nil, context: {}).resolve(args)
+    def perform_superuser(venue: nil, **args)
+      Mutations::CreateVenue.new(object: nil, context: {current_user:{role: 3}}).resolve(args)
+    end
+
+    def perform_other_user(venue: nil, **args)
+      Mutations::CreateVenue.new(object: nil, context: {current_user:{role: 2}}).resolve(args)
     end
 
     it 'create a new venue' do
-      venue = perform(
+
+      venue = perform_superuser(
         name: 'Venue One',
         street1: '123 main',
         city: 'Denver',
@@ -18,6 +23,18 @@ class Mutations::CreateUserTest < ActiveSupport::TestCase
       assert venue.persisted?
       assert_equal venue.name, 'Venue One'
       assert_equal venue.addresses[0].city, 'Denver'
+    end
+
+    it 'throws error when current user is not superuser' do
+      venue = perform_other_user(
+        name: 'Venue One',
+        street1: '123 main',
+        city: 'Denver',
+        state: 'CO',
+        zip: '80128'
+      )
+
+      assert_equal venue, {:error=> "Not Authorized to create venue!"}
     end
   end
 end
