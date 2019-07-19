3d735a5c403a390c0b0aadd054ec8a34aa76bcb7
Matt Weiss <weiss.matt@outlook.com>
2019-07-18 20:17:56 -0600

    Adds test for many to many relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ describe User, type: :model do
   describe 'validations' do
     it{should validate_presence_of(:username)}
     it{should validate_presence_of(:password)}
+    it{should have_many(:venues)}
   end
 end
