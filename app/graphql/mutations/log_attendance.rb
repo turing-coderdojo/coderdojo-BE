da19620fcf15d59dcdc7738613cb10e13e615e40
Matt Weiss <weiss.matt@outlook.com>
2019-07-20 15:37:57 -0600

    Adds check for student that has already logged attendance to the event

--- app/graphql/mutations/log_attendance.rb
+++ app/graphql/mutations/log_attendance.rb
@@ -10,15 +10,16 @@ module Mutations
 
       current_user = context[:current_user]
       event = Event.find(event_id)
+      attendance = UserEvent.find_by(user_id: current_user.id, event_id: event_id)
 
-      if (current_user && (current_user[:role] == 0))
+      if (current_user && (current_user[:role] == 0) && attendance == nil)
         if event.event_code == event_code
           UserEvent.create(user_id: current_user.id, event_id: event_id)
         else
           GraphQL::ExecutionError.new("Incorrect Event Code!")
         end
       else
-        GraphQL::ExecutionError.new("Not authorized to promote users!")
+        GraphQL::ExecutionError.new("Cannot log attendance!")
       end
     end
   end
