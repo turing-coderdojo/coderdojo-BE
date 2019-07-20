641f01222b2c13d3a5e9e5107493b9a5c1c10964
Matt Weiss <weiss.matt@outlook.com>
2019-07-20 14:57:44 -0600

    Adds event_code to Event type

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -6,6 +6,7 @@ module Types
     field :notes, String, null: true
     field :start_time, String, null: false
     field :end_time, String, null: false
+    field :event_code, String, null: true
     field :addresses, [AddressType], null: true, method: :addresses
     field :venue, VenueType, null: true, method: :venue
   end
