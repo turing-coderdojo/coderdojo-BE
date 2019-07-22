f2354b2fcfe5c7121c0a41a367ad490f23b9dbff
Matt Weiss <weiss.matt@outlook.com>
2019-07-22 08:48:36 -0600

    Adds achievement_goal relationship tests to relevant model specs

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Achievement, type: :model do
   describe 'relationships' do
     it{should belong_to :venue}
+    it{should have_many :achievement_goals}
   end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Goal, type: :model do
     it{should validate_presence_of :category}
     it{should validate_presence_of :name}
     it{should validate_presence_of :description}
+    it{should have_many :achievement_goals}
   end
 end
