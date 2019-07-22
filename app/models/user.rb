9d501f7598b15c6942c3771ff84059d4ab5853dd
Matt Weiss <weiss.matt@outlook.com>
2019-07-22 15:37:53 -0600

    Implements bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates_presence_of :username
-  validates_presence_of :password
   validates_uniqueness_of :username
 
   has_many :students, class_name: "User",
