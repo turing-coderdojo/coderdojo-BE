293b66aa6ad81c69c46981e983cb5b613107d00c
Matt Weiss <weiss.matt@outlook.com>
2019-07-22 08:49:10 -0600

    Adds relationships to achievement and goal models

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,6 @@ class Goal < ApplicationRecord
   validates_presence_of :category
   validates_presence_of :name
   validates_presence_of :description
+  has_many :achievement_goals
+  has_many :achievements, through: :achievement_goals
 end
