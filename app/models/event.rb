f7f1f08410d54d052589b8ad40538c9628328303
Matt Weiss <weiss.matt@outlook.com>
2019-07-23 15:01:15 -0600

    Changes 'verifies' to 'validates'

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   validates_presence_of :start_time
   validates_presence_of :end_time
 
-  verify_uniqueness_of :event_code
+  validates_uniqueness_of :event_code
 
   belongs_to :venue
   has_many :addresses, :as => :addressable
