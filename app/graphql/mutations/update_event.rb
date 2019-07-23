5c7bfcdecb5b554253227b3bf75a79b89a0ea4ed
Matt Weiss <weiss.matt@outlook.com>
2019-07-23 16:09:22 -0600

    Adds mutation for updating events

---
+++ app/graphql/mutations/update_event.rb
@@ -0,0 +1,56 @@
+module Mutations
+  class UpdateEvent < Mutations::BaseMutation
+    #Event args
+    argument :id, String, required: true
+    argument :name, String, required: false
+    argument :notes, String, required: false
+    argument :start_time, String, required: false
+    argument :end_time, String, required: false
+    argument :venue_id, Integer, required: false
+    #Address args
+    argument :street_1, String, required: false
+    argument :street_2, String, required: false
+    argument :city, String, required: false
+    argument :state, String, required: false
+    argument :zip, String, required: false
+
+    type Types::EventType
+
+    def resolve(id: nil,
+              name: nil,
+             notes: nil,
+        start_time: nil,
+          end_time: nil,
+          venue_id: nil,
+           street1: nil,
+           street2: nil,
+              city: nil,
+             state: nil,
+               zip: nil)
+
+      current_user = context[:current_user]
+
+      event = Event.find(id)
+
+      if (current_user && current_user.is_venue_admin?(venue_id)) ||
+        (current_user && (current_user[:role] == 3))
+        event.update(name: name,
+                            notes: notes,
+                       start_time: start_time,
+                         end_time: end_time,
+                         venue_id: venue_id)
+        if street1
+          event.addresses.update(
+            street_1: street1,
+            street_2: street2,
+                city: city.downcase,
+               state: state,
+                 zip: zip)
+        end
+        return event
+      else
+        GraphQL::ExecutionError.new("Not authorized to update event!")
+      end
+    end
+  end
+end
