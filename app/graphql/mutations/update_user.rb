519d7f8ea36461602a416a0c8cd9c311641fe351
Matt Weiss <weiss.matt@outlook.com>
2019-07-24 18:02:13 -0600

    Adds logic for handling different types of students.

--- app/graphql/mutations/update_user.rb
+++ app/graphql/mutations/update_user.rb
@@ -1,12 +1,13 @@
 module Mutations
   class UpdateUser < Mutations::BaseMutation
     #User args
-    argument :id, String, required: false
+    argument :id, Integer, required: false
     argument :email, String, required: false
     argument :username, String, required: false
     argument :password, String, required: false
     argument :name, String, required: false
     argument :phone_number, String, required: false
+    argument :birthdate, String, required: false
 
     field :token, String, null: true
     field :user, Types::UserType, null: true
@@ -20,12 +21,18 @@ module Mutations
          birthdate: nil)
 
       current_user = context[:current_user]
-      student = User.find(id)
+
+      if id
+        student = User.find(id)
+      elsif current_user.role == 0
+        student = current_user
+      end
 
       student_params = {birthdate: birthdate,
         username: username,
         password: password,
-        name: name}
+        name: name,
+        role: 0}
 
       user_params = {email: email,
           username: username,
@@ -33,21 +40,26 @@ module Mutations
           name: name,
           phone_number: phone_number}
 
-      if current_user.guardian_id
+      if current_user.guardian_id != nil
         GraphQL::ExecutionError.new("Not Authorized to update!")
-      elsif (current_user.role == 0) || current_user.students.includes?(student)
+      elsif student || current_user.students.include?(student)
         student.update(student_params)
         user = student
+        token = Base64.encode64(user.username)
+        {
+          token: token,
+          user: user
+        }
       else
         current_user.update(user_params)
         user = current_user
+        token = Base64.encode64(user.username)
+        {
+          token: token,
+          user: user
+        }
       end
 
-      token = Base64.encode64(user.username)
-      {
-        token: token,
-        user: user
-      }
     end
 
   end
