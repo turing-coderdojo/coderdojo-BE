d674b85e1eb75cc37c46c49961eb5a410c8ade34
Matt Weiss <weiss.matt@outlook.com>
2019-08-02 23:06:16 -0600

    Changes sign_in mutation to accept email or username for sign in

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -1,14 +1,20 @@
 module Mutations
   class SignIn < Mutations::BaseMutation
-    argument :username, String, required: true
+    argument :username, String, required: false
+    argument :email, String, required: false
     argument :password, String, required: true
 
     field :token, String, null: true
     field :user, Types::UserType, null: true
 
     def resolve(username: nil, password: nil)
-      user = User.find_by(username: username)
-      return GraphQL::ExecutionError.new("Invalid Username!") unless user
+
+      user = User.find_by(email: username)
+      if !user
+        user = User.find_by(username: username)
+      end
+
+      return GraphQL::ExecutionError.new("Invalid Email/Username!") unless user
       return GraphQL::ExecutionError.new("Invalid Password!") unless user.authenticate(password)
 
       token = Base64.encode64(user.username)
